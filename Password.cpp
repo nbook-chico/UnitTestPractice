a391390afdf684718bead2a5ac770d69f79e7395
nbook-chico <nbook@csuchico.edu>
2025-11-05 11:41:09 -0800

    Added tests

--- Password.cpp
+++ Password.cpp
@@ -10,6 +10,9 @@ using std::string;
   'z' and any ASCII characters are allowed.
 */
 int Password::count_leading_characters(string phrase){
+  // Initial code didn't account for empty strings
+  if(phrase.empty()) return 0;
+
   int repetition = 1;
   int index = 0;
   while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
@@ -25,5 +28,14 @@ int Password::count_leading_characters(string phrase){
   letter and at least one lower-case letter
 */
 bool Password::has_mixed_case(string pass){
+  // This was totally unimplemented  
+  bool hasUpper = false;
+  bool hasLower = false;
+
+  for(char c : pass){
+    if (isupper(c)) hasUpper = true;
+    if (islower(c)) hasLower = true;
+    if (hasUpper && hasLower) return true;
+  }
   return false;
 }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,16 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+
+TEST(PasswordTest, multi_leading_letters)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("aab"));
+	ASSERT_EQ(2, my_password.count_leading_characters("aabb"));
+	ASSERT_EQ(1, my_password.count_leading_characters("Aab"));
+	ASSERT_EQ(1, my_password.count_leading_characters("aAb"));
+	ASSERT_EQ(4, my_password.count_leading_characters("aaaa"));
+	ASSERT_EQ(2, my_password.count_leading_characters("aaba"));
+	ASSERT_EQ(2, my_password.count_leading_characters("aaAa"));
+	ASSERT_EQ(1, my_password.count_leading_characters("aba"));
+}
